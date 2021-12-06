f9d921fb9402dfc522b25d2502b73e33aec42b59
Wach <mottymu@gmail.com>
2021-12-06 15:54:29 +0700

    lecture32 - 33

---
+++ lecture32.py
@@ -0,0 +1,10 @@
+"""
+name = input("What's your first name ?")
+lastname = input("What's your last name ?")
+print("Hello", name, lastname)
+"""
+# ---
+firstNumber = input("First Number :")
+secondNumber = input("Second Number :")
+result = int(firstNumber) + int(secondNumber)
+print("Result :", result)
