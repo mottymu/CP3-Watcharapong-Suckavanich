8210b5424f5887a9da17ed0046cb418e1c99a70c
Wach <mottymu@gmail.com>
2021-12-06 15:28:03 +0700

    lecture28 - 31

---
+++ lecture29.py
@@ -0,0 +1,16 @@
+"""
+a = 10
+b = 12
+c = a + b
+a += 5  # a = a + 5
+print(a)
+# ---
+money = 1000
+money = money - 50
+money = money - 20
+print(money)
+"""
+money = 1000
+money -= 50
+money -= 20
+print(money)
---
+++ lecture31.py
@@ -0,0 +1,11 @@
+a = "Wach"
+b = "S"
+print(a, b)
+# ---
+a = "10"
+b = 5
+c = int(a) + b
+print(c)
+# ---
+c = float(a) + b
+print(c)
