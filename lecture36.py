3400b904d98e18cd578ce6f999b7b1ccc86a66f8
Wach <mottymu@gmail.com>
2021-12-08 16:38:43 +0700

    lecture36 - lecture 37

---
+++ lecture36.py
@@ -0,0 +1,12 @@
+"""
+if False:
+    print("Hello Condition 1!")
+print("Hello Condition 2!")
+"""
+
+money = 1000
+if money > 300:
+    print("Go to KFCCC !!!")
+else:
+    print("So Saddd !! T_T")
+print("Bye Bye")
