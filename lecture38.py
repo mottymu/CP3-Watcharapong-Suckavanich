01ba4723d415f08ec7f2e01ecd6232d3c252cce1
Wach <mottymu@gmail.com>
2021-12-26 18:25:21 +0700

    lecture 38

---
+++ lecture38.py
@@ -0,0 +1,12 @@
+if False:
+    print("First Condition")
+    print("First Condition")
+    print("First Condition")
+    print("First Condition")
+    print("First Condition")
+elif True:
+    print("Second Condition")
+elif False:
+    print("Third Condition")
+else:
+    print("Else Condition")
