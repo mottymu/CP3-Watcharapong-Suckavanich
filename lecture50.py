1f7425ed1f166ce888840984fed20c10cbe72a39
Wach <mottymu@gmail.com>
2021-12-31 16:59:58 +0700

    lecture 50

---
+++ lecture50.py
@@ -0,0 +1,21 @@
+def addNumber(x, y):
+    print(x + y)
+
+
+def minusNumber(x, y):
+    print(x - y)
+
+
+def multiplyNumber(x, y):
+    print(x * y)
+
+
+def divideNumber(x, y):
+    print(x / y)
+
+
+addNumber(100, 25)
+minusNumber(100, 25)
+multiplyNumber(100, 25)
+divideNumber(100, 25)
+
