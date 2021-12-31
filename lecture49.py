bcb1e7658874d40d1629b05045f5bbe6f0e74a38
Wach <mottymu@gmail.com>
2021-12-31 16:50:15 +0700

    lecture 49

---
+++ lecture49.py
@@ -0,0 +1,12 @@
+def sayHelloWorld():
+    print("Hello World")
+    sayHelloMe()
+    print("Woooooooo!")
+def sayHelloMe():
+    print("Hello Wach")
+    print("Hahaa")
+    sayHelloWorld()
+
+sayHelloWorld()
+
+sayHelloMe()
