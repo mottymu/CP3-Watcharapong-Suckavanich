8a03795322ad5985e09672498c1d23450c6d1f10
Wach <mottymu@gmail.com>
2021-12-31 16:35:50 +0700

    Tips

---
+++ Tips.py
@@ -0,0 +1,13 @@
+# Tips
+
+# การ Print ข้อความให้อยู่ในบรรทัดเดียวกัน
+
+for i in range(5):
+    print(i, end = "")
+print()
+for i in range(5):
+    print(i, end = " ")
+print()
+for i in range(5):
+    print(i, end = "-")
+print()
