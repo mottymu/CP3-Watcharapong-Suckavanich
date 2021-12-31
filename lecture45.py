9c1df930bcf830d46fedf94b0e5bb1bb07d07fa4
Wach <mottymu@gmail.com>
2021-12-31 14:10:05 +0700

    lecture 45

---
+++ lecture45.py
@@ -0,0 +1,9 @@
+# for loop example
+
+inputRound = int(input("Please Enter Number : "))
+sum = 0
+print(list(range(inputRound)))
+for x in range(inputRound):
+    inputNumber = int(input("x" + str(x+1) + ": "))
+    sum = sum + inputNumber
+print(sum)
