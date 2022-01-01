acb806d117cbddf2e126f987fdf15ba864bc214e
Wach <mottymu@gmail.com>
2022-01-01 14:51:51 +0700

    lecture 59

---
+++ lecture59.py
@@ -0,0 +1,11 @@
+# String Function Example
+
+name = input("Firstname : ").capitalize()
+lastname = input("Lastname : ").capitalize()
+print("Hello !! %s %s !" % (name, lastname))
+
+text = "Wach"
+textFormatted = "Welcome %s" % (name)
+print(textFormatted.center(30, "-"))
+print(len(name))
+
