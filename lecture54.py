39d1b728b0ce353561b7557bbb7bc9895c5fe0d9
Wach <mottymu@gmail.com>
2022-01-01 13:57:44 +0700

    lecture 54 homework

---
+++ lecture54.py
@@ -0,0 +1,50 @@
+# Return Function Example 3
+
+# Log in
+def login():
+    usernameInput = input("Username :")
+    passwordInput = input("Password :")
+    if usernameInput == "admin" and passwordInput == "1234":
+        return showMenu()
+    else:
+        print("Invalid Username or Password")
+        return login()
+
+
+# Show Menu
+def showMenu():
+    print("-----iShop-----")
+    print("1. Vat Calculator")
+    print("2. Price Calculator")
+    menuSelect()
+
+
+# Select Menu
+def menuSelect():
+    userSelected = int(input(">>"))
+    if userSelected == 1:
+        totalPrice = int(input("Total Price (THB) :"))
+        print(vatCalculate(totalPrice))
+    elif userSelected == 2:
+        print(priceCalculate())
+    else:
+        print("Please choose.....")
+        menuSelect()
+
+# VAT Calculator
+def vatCalculate(totalPrice):
+    vat = 7
+    result = totalPrice + (totalPrice * vat / 100)
+    return result
+
+
+# Price Calculator
+def priceCalculate():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    return vatCalculate(price1 + price2)
+
+
+#  Homework
+
+login()
