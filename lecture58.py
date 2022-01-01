12a736aa22dfe89a05f92c1c9fa5ad0a7d1d8a0c
Wach <mottymu@gmail.com>
2022-01-01 14:36:58 +0700

    lecture 58

---
+++ lecture58.py
@@ -0,0 +1,9 @@
+# String Format Example
+
+name = "Wach"
+weight = "66"
+print("Hello" + name + " ! and weight is " + weight + "kg")
+print("Hello %s ! and weight is %s kg" % (name, weight))
+print("Hello %s ! and weight is %d kg" % (name, 66))
+print("Hello %s ! and weight is %d kg" % (name, 66.66))
+print("Hello %s ! and weight is %f kg" % (name, 66.66))
