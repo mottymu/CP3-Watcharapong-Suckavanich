1780690a497859c555b9591c1996866c043ca7c6
Wach <mottymu@gmail.com>
2022-01-01 15:50:55 +0700

    lecture 67

---
+++ lecture67.py
@@ -0,0 +1,10 @@
+# Tuple Function Example 2
+
+tupleEx = (1, 200, 3, 4, 10)
+listEx = [1, 2, 3, 4, 10]
+print(len(tupleEx))
+print(len(listEx))
+print(min(tupleEx))
+print(max(tupleEx))
+print(tuple(listEx))
+print(tupleEx.index(200))
