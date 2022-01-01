dde95df31701045fc23e56f22e50c02a672b824b
Wach <mottymu@gmail.com>
2022-01-01 14:27:48 +0700

    lecture 57

---
+++ lecture57.py
@@ -0,0 +1,13 @@
+# String Example
+
+text = "Hello"
+print(text[0])
+print(text[0] + "Hello")
+print(text[0] + text[1] + text[2] + text[3])
+print(text[0:4])  # เอาตัวที่ 0 ถึงก่อนหน้า 4
+
+address = "642/17 Sukhumvit Bangkok"
+print("Bangkok" in address)
+print("Bkk" in address)
+print("Sukhumvit" in address)
+print("Sukhumvit" not in address)
