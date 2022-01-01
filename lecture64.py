dc51d4516af91be44d1b661c366e3f5b94c6c32b
Wach <mottymu@gmail.com>
2022-01-01 15:31:14 +0700

    lecture 64

---
+++ lecture64.py
@@ -0,0 +1,11 @@
+# List Function Example
+
+myFriends = ["AA", "BB", "CC"]
+print(myFriends)
+myFriends[2] = "XX"
+myFriends.append("DD")
+print(myFriends)
+myFriends.remove("BB")
+print(myFriends)
+del myFriends[0]
+print(myFriends)
