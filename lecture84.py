5340658c13ce9e452834a97d1d5f7c030ac2634c
Wach <mottymu@gmail.com>
2022-01-03 15:28:19 +0700

    lecture 84 with demo.txt

---
+++ lecture84.py
@@ -0,0 +1,9 @@
+# Read File Example
+
+file = open("demo.txt", "r")
+# print(file.read())
+# print(file.read(14))
+print(file.readline())
+print(file.readline())
+print(file.readline())
+print(file.readline())
