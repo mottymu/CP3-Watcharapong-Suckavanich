4d703dddcf15210375778c0fbbbed055c04c07bf
Wach <mottymu@gmail.com>
2022-01-03 15:06:44 +0700

    lecture 82

---
+++ lecture82.py
@@ -0,0 +1,15 @@
+# Assert Example
+
+'''
+x = "hello"
+
+assert x == "goodbye", "x should be 'hello'"
+'''
+
+for i in range(10):
+    print(i)
+    assert i < 5, "Error"
+
+for i in range(10):
+    assert i < 5, "Error"
+    print(i)
