ba2c75ec8bf4f78f6cb37bc9c15e48fc905dd3f0
Wach <mottymu@gmail.com>
2022-01-04 15:22:45 +0700

    lecture 98

---
+++ lecture98.py
@@ -0,0 +1,19 @@
+# Event Driven Example
+
+from tkinter import *
+
+
+def leftClickButton(event):
+    print("Left Click !!!")
+
+
+def doubleClickButton(event):
+    print("Double Click !!!")
+
+
+main = Tk()
+button = Button(main, text="My Button !!")
+button.pack()
+button.bind('<Button-1>',leftClickButton)
+button.bind('<Double-1>',doubleClickButton)
+main.mainloop()
