d6fca0d5e5002d672dab41482a89c486763a9cc2
Wach <mottymu@gmail.com>
2022-01-04 14:45:24 +0700

    lecture 95

---
+++ lecture95.py
@@ -0,0 +1,15 @@
+# Widget Size Example
+
+from tkinter import *
+
+
+def sayHelloWorld():
+    print("Hello World")
+
+
+mainWindow = Tk()
+button = Button(mainWindow, text="Click Me 1", command=sayHelloWorld,width=20).grid(row=2, column=1)
+button2 = Button(mainWindow, text="Click Me 2", command=sayHelloWorld).grid(row=1, column=1)
+button2 = Button(mainWindow, text="Click Me 3", command=sayHelloWorld).grid(row=0, column=2)
+label = Label(mainWindow,text="Hello World",width=20).grid(row=0,column=1)
+mainWindow.mainloop()
